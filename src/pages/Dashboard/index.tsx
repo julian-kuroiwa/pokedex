c2a32c1270af73c142418ceefec4ab581e1191dd
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-05-01 16:25:20 -0300

    Header on dashboard
    Created Header for dashboard.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Header } from './styles';
 
 import Logo from '../../assets/logo-white.svg';
 import LogoPokemon from '../../assets/logo-pokemon.svg';
 
 const Dashboard: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Header>
+        <div>
+          <img src={Logo} alt="Logo Hanzo" />
+          <div />
+        </div>
+      </Header>
+    </Container>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -2,3 +2,22 @@ import styled from 'styled-components';
 import { shade } from 'polished';
 
 export const Container = styled.div``;
+
+export const Header = styled.header`
+  background-color: var(--primary-color);
+  height: 60px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  > div {
+    max-width: 1024px;
+    width: 100%;
+    padding: 0 20px;
+    margin: 0 auto;
+
+    > img {
+      height: 40px;
+    }
+  }
+`;
