ddd2e89dce8dd0a8f4d1f580d0cfa3dd2ff734d8
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-05-01 13:48:50 -0300

    Initial Setup
    Installed and configured all dependencies needed to start this project.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { GlobalStyle } from './stlyes/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  devtool: 'source-map',
+  entry: "./src/index.tsx",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  devServer: {
+      inline: true,
+      contentBase: './public',
+      port: 3000
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+        {
+            test: /\.(ts|tsx)$/,
+            loader: 'ts-loader'
+        },
+        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  plugins:[
+      new HtmlWebPackPlugin({
+          template: "./public/index.html",
+          filename: "index.html"
+      }),
+  ]
+  };
